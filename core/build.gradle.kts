f6f7253e1da37d081186ca5d063f14b35725ae11
Tóth István Zoltán <tiz@simplexion.hu>
2021-04-07 08:48:04 +0200

    2021.4.7

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "hu.simplexion.zakadabar"
-version = "2021.4.6-SNAPSHOT"
+version = "2021.4.7"
 
 val isSnapshot = version.toString().contains("SNAPSHOT")
 
--- demo/demo/build.gradle.kts
+++ demo/demo/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "hu.simplexion.zakadabar"
-version = "2021.4.6-SNAPSHOT"
+version = "2021.4.7"
 
 application {
     mainClassName = "zakadabar.stack.backend.ServerKt"
