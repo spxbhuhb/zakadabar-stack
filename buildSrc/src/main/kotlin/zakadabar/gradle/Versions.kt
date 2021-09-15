11cf28389c430030304cc68cf22a38ee4f04036a
Tóth István Zoltán <tiz@simplexion.hu>
2021-09-15 12:02:47 +0200

    2021.9.15-RC1

--- buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package zakadabar.gradle
 
 object Versions {
-    const val zakadabar = "2021.9.7-SNAPSHOT"
+    const val zakadabar = "2021.9.15"
 
     val isSnapshot = zakadabar.contains("SNAPSHOT")
 
