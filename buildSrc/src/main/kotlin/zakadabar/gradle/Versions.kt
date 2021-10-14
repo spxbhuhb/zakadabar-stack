8d00f76856e1785191f07d4305aee3798e050efc
Istvan Zoltan Toth <tiz@simplexion.hu>
2021-10-14 06:58:43 +0200

    bump to 2021.10.14-SNAPSHOT

--- buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package zakadabar.gradle
 
 object Versions {
-    const val zakadabar = "2021.10.12"
+    const val zakadabar = "2021.10.14-SNAPSHOT"
 
     val isSnapshot = zakadabar.contains("SNAPSHOT")
 
