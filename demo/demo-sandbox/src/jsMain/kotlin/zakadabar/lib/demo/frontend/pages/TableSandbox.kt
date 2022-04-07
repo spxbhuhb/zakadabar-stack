17b37b2aed59fd50299f614630196cdc8d8de177
Istvan Zoltan Toth <tiz@simplexion.hu>
2022-04-07 08:04:42 +0200

    playing around with table
    Signed-off-by: Istvan Zoltan Toth <tiz@simplexion.hu>

--- demo/demo-sandbox/src/jsMain/kotlin/zakadabar/lib/demo/frontend/pages/TableSandbox.kt
+++ demo/demo-sandbox/src/jsMain/kotlin/zakadabar/lib/demo/frontend/pages/TableSandbox.kt
@@ -3,40 +3,21 @@
  */
 package zakadabar.lib.demo.frontend.pages
 
+import kotlinx.browser.document
 import kotlinx.browser.window
-import org.w3c.dom.HTMLElement
-import org.w3c.dom.get
-import org.w3c.dom.set
+import org.w3c.dom.*
 import zakadabar.core.browser.ZkElement
 import zakadabar.core.browser.page.ZkPage
+import zakadabar.core.browser.table.ZkTableStyles
 import zakadabar.core.browser.util.log
 import zakadabar.core.browser.util.w3c.IntersectionObserver
 import zakadabar.core.browser.util.w3c.IntersectionObserverEntry
-import zakadabar.core.resource.ZkColors
-import zakadabar.core.resource.css.*
+import zakadabar.core.resource.css.cssStyleSheet
+import zakadabar.core.resource.css.px
 
-var sandboxStyles by cssStyleSheet(SandboxStyles())
+var styles by cssStyleSheet(SandboxStyles())
 
-open class SandboxStyles : ZkCssStyleSheet() {
-
-    open var defaultRowHeight by cssParameter { 50.px }
-
-    open val page by cssClass {
-        backgroundColor = ZkColors.Blue.a200
-    }
-
-    open val container by cssClass {
-        + OverflowY.scroll
-        margin = 20.px
-        height = 500.px
-    }
-
-    open val row by cssClass {
-        minHeight = defaultRowHeight
-        paddingLeft = 10.px
-        borderBottom = "1px solid black"
-        backgroundColor = ZkColors.white
-    }
+open class SandboxStyles : ZkTableStyles() {
 
 }
 
@@ -44,7 +25,10 @@ class TableSandbox : ZkPage() {
 
     val data = (1..500).map { it - 1 }
 
-    val container = ZkElement()
+    val contentContainer = ZkElement()
+    lateinit var tableElement: HTMLTableElement
+    lateinit var tableBodyElement : HTMLTableSectionElement
+
     val content = ZkElement()
 
     val state = TableState()
@@ -55,51 +39,78 @@ class TableSandbox : ZkPage() {
         state.estimatedTotalHeight = data.size * state.estimatedRowHeight
         state.rowStates = arrayOfNulls(data.size)
 
+        + styles.outerContainer
 
-        + sandboxStyles.page
+        + contentContainer.apply {
+            + styles.contentContainer
 
-        + container.apply {
-
-            + sandboxStyles.container
-
-            + content.apply {
-                height = state.estimatedTotalHeight.px
-                (0..15).forEach {
-                    attachRow(it)
+            + table(styles.table) {
+                buildPoint.style.cssText = "grid-template-columns: repeat(10, 100px)"
+                + thead {
+                    + styles.noSelect
+                    repeat(10) { + th { + it.toString() } }
                 }
+                + tbody {
+
+                }.also {
+                    tableBodyElement = it
+                }
+            }.also {
+                tableElement = it
             }
         }
+
+        (0..15).forEach {
+            attachRow(it)
+        }
+
+        val bottomPlaceHolder = ZkElement(document.createElement("tr") as HTMLElement) build {
+            repeat(10) { + td { height = state.estimatedTotalHeight.px } }
+        }
+
+        tableBodyElement.appendChild(bottomPlaceHolder.element)
     }
 
-    fun ZkElement.attachRow(index: Int) {
+    /**
+     * Attaches a row to the table. Attached rows are not necessarily visible for
+     * the user (they may be scrolled out) but they are added to the DOM.
+     */
+    fun attachRow(index: Int) {
+        // Get the state of the row or create a new one if it doesn't exist yet
         val rowState = state.rowStates[index] ?: RowState().also { state.rowStates[index] = it }
 
+        // Render and attach the row
         rowState.apply {
-            ensureRender(index)
+            ensureRender(index).also {
+                tableBodyElement.appendChild(it.element)
+                observer.observe(it.element)
+            }
             attached = true
         }
-
     }
 
-    fun RowState.ensureRender(index: Int) {
-        if (render != null) return
+    /**
+     * Renders the row. Caches the result and returns with it for subsequent calls
+     * on the same row.
+     *
+     * @param index Index of the row in state.rowStates
+     */
+    fun RowState.ensureRender(index: Int): ZkElement {
+        render?.let { return it }
 
-        render = ZkElement() build {
-            + row {
-                + sandboxStyles.row
-                height = (50 + (index % 20) * 2).px
+        render = ZkElement(document.createElement("tr") as HTMLElement) build {
 
-                repeat(10) {
-                    + div {
-                        + "row ${data[index]}"
-                    }
+            repeat(10) {
+                + td {
+                    height = (50 + (index % 20) * 2).px
+                    + "cell ${data[index]}:$it"
                 }
-                element.dataset["zkri"] = index.toString()
             }
+
+            element.dataset["zkri"] = index.toString()
         }
 
-        this@TableSandbox.content += render
-        observer.observe(render !!.element)
+        return render !!
     }
 
     fun observerCallback(entries: Array<IntersectionObserverEntry>, o: IntersectionObserver) {
@@ -113,12 +124,13 @@ class TableSandbox : ZkPage() {
                 if (it.isIntersecting && target.isLast() && rowIndex + 1 < data.size) {
                     attachRow(rowIndex + 1)
                 }
+                println(it.target.id + " " + it.isIntersecting)
             }
             window.requestAnimationFrame {
                 state.rowStates.forEach { rs ->
                     rs?.render?.let {
                         rs.height = it.element.getBoundingClientRect().height
-                        if (!rs.adjusted && rs.height != state.estimatedRowHeight) {
+                        if (! rs.adjusted && rs.height != state.estimatedRowHeight) {
                             state.estimatedTotalHeight += (rs.height - state.estimatedRowHeight)
                             rs.adjusted = true
                             println("height: ${state.estimatedTotalHeight}")
@@ -132,11 +144,11 @@ class TableSandbox : ZkPage() {
         }
     }
 
-    fun HTMLElement.isLast() = (nextElementSibling == null)
+    fun HTMLElement.isLast() = (nextElementSibling?.nextElementSibling == null)
 
     fun makeObserver(): IntersectionObserver {
         val options: dynamic = object {}
-//        options["root"] = element.parentElement
+        options["root"] = element
 //        options["rootMargin"] = "200px" // pre-load when the area is as close as 200px to the screen
 //        options["threshold"] = 0 // pre-load when even a pixel of the area is about to be shown
 
