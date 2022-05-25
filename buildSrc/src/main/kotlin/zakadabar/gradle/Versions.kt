763031e30639166eac840e3473be3c580e4fa9d1
Istvan Zoltan Toth <tiz@simplexion.hu>
2022-05-25 08:22:02 +0200

    release-2022.5.25
    Signed-off-by: Istvan Zoltan Toth <tiz@simplexion.hu>

--- buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package zakadabar.gradle
 
 object Versions {
-    const val zakadabar = "2022.5.24-SNAPSHOT"
+    const val zakadabar = "2022.5.25"
 
     val isSnapshot = zakadabar.contains("SNAPSHOT")
 
