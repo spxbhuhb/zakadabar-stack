025feeac97084796ebc4768ab9986f49a988691c
Istvan Zoltan Toth <tiz@simplexion.hu>
2022-09-08 04:15:36 +0200

    version bump
    Signed-off-by: Istvan Zoltan Toth <tiz@simplexion.hu>

--- buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package zakadabar.gradle
 
 object Versions {
-    const val zakadabar = "2022.9.7"
+    const val zakadabar = "2022.9.8-SNAPSHOT"
 
     val isSnapshot = zakadabar.contains("SNAPSHOT")
 
