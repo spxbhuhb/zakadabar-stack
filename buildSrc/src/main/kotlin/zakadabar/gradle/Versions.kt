487fb808a59a62604de47648757c34fabf41a840
Tóth István Zoltán <tiz@simplexion.hu>
2023-02-10 10:49:35 +0100

    version bump
    Signed-off-by: Tóth István Zoltán <tiz@simplexion.hu>

--- buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/zakadabar/gradle/Versions.kt
@@ -4,7 +4,7 @@
 package zakadabar.gradle
 
 object Versions {
-    const val zakadabar = "2023.2.10-SNAPSHOT"
+    const val zakadabar = "2023.2.10"
 
     val isSnapshot = zakadabar.contains("SNAPSHOT")
 
